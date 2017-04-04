31bb062ba19db39deea0b8f1fd2d8e968992679d
Madiyar09 <aitmadiar@gmail.com>
2017-04-05 00:55:47 +0600

    Update SignUp.java

--- Client/SignUp.java
+++ Client/SignUp.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package P2P_Client;
 
 import java.io.BufferedReader;
@@ -15,10 +10,6 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author aibek
- */
 public class SignUp extends javax.swing.JFrame {
 
     /**
