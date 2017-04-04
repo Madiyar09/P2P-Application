fa36b8390d2ea764c6ddfa83c52e7f653271b0b0
Madiyar09 <aitmadiar@gmail.com>
2017-04-05 00:54:55 +0600

    Update P2P_Client.java

--- Client/P2P_Client.java
+++ Client/P2P_Client.java
@@ -15,17 +15,6 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.swing.table.DefaultTableModel;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author aibek
- */
-
 class Info {
     public String Name;
     public String Type;
