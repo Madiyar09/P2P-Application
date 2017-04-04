c5b0b33146c33cfbb2b005f34c2f7fd70b6b12f9
Madiyar09 <aitmadiar@gmail.com>
2017-04-05 00:56:24 +0600

    Update P2P_Server.java

--- Server/P2P_Server.java
+++ Server/P2P_Server.java
@@ -1,11 +1,5 @@
 package p2p_server;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import P2P_Client.Encryption;
 import P2P_Client.FilePath;
 import java.io.*;
