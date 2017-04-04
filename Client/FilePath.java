a310ca8466137b99ee9ec7a76e4da25096a932c8
Madiyar09 <aitmadiar@gmail.com>
2017-04-05 00:54:07 +0600

    Update FilePath.java

--- Client/FilePath.java
+++ Client/FilePath.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package P2P_Client;
 
 import java.io.File;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author aibek
- */
 public class FilePath extends javax.swing.JFrame {
     
     public static File SharedPath;
